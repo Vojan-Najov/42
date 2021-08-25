3d3c35176385890f9460a1df402c24eb546c6c40
Vojan-Najov <serejah.mashichev@gmail.com>
2021-08-25 18:48:28 +0300

    fix test ft_printf, add return values test

--- exam02/ft_printf/test.c
+++ exam02/ft_printf/test.c
@@ -5,30 +5,36 @@ int	ft_printf(const char *fmt, ...);
 
 int	main(int argc, char **argv)
 {
+	int	a;
+	int	b;
+
 	if (argc == 3 || argc == 4)
 	{
 		if (argv[1][0] == 's')
 		{
-			printf(argv[2], argv[3]);
+			a = printf(argv[2], argv[3]);
 			printf("\n");
-			ft_printf(argv[2], argv[3]);
+			b = ft_printf(argv[2], argv[3]);
 			printf("\n");
+			printf("p = %d   f = %d\n", a, b);
 		}
 		else if (argv[1][0] == 'd')
 		{
 			int n = atoi(argv[3]);
-			printf(argv[2], n);
+			a = printf(argv[2], n);
 			printf("\n");
-			ft_printf(argv[2], n);
+			b = ft_printf(argv[2], n);
 			printf("\n");
+			printf("p = %d   f = %d\n", a, b);
 		}
 		else if (argv[1][0] == 'x')
 		{
 			unsigned int n = atoi(argv[3]);
-			printf(argv[2], n);
+			a = printf(argv[2], n);
 			printf("\n");
-			ft_printf(argv[2], n);
+			b = ft_printf(argv[2], n);
 			printf("\n");
+			printf("p = %d   f = %d\n", a, b);
 		}
 	}
 	return (0);
